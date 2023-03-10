83e70217a13978b3fd7b6a9ebf158e32bc41dd03
Amaroke <70974789+Amaroke@users.noreply.github.com>
2023-03-10 16:40:51 +0100

    Push de la base

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import './App.css';
 import Button from "./components/Button/Button";
 
 function App() {
-  return (
-    <div>
-      Bonjour !
-        <Button/>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <select>
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                </select>
+            </header>
+            <main>
+                <div>
+                    <img src="image1.jpg" alt="Image 1" />
+                    <button>Button 1</button>
+                </div>
+                <div>
+                    <img src="image2.jpg" alt="Image 2" />
+                    <button>Button 2</button>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
