df435c88a6bfbd188d5772656b077d758a9e03a3
Amaroke <70974789+Amaroke@users.noreply.github.com>
2023-03-10 16:24:33 +0100

    First commit

---
+++ src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import "./Button.css";
+
+export default function Button() {
+    return (
+        <button className="center red"
+                type="button">
+            Add to favorites
+        </button>
+    );
+}
