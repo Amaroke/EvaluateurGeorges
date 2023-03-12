77638695b2d4c989e8750c2f9ec0f5d9a0eab84e
Amaroke <70974789+Amaroke@users.noreply.github.com>
2023-03-12 15:12:06 +0100

    Suppression des components, pour une gestion simple des images sur une page statique

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import './App.css';
-import Button from "./components/Button/Button";
-import Image from "./components/Image/Image";
-import Menu from "./components/Menu/Menu";
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Cookies from 'js-cookie';
+import {TransformComponent, TransformWrapper} from "react-zoom-pan-pinch";
+import './App.css';
 
+// Liste de tous les noms des images utilisées
+const imageNames = [
+    "barb_0001-4133_1919_num_5_1_T1_0021_0000",
+    "barb_0001-4133_1919_num_5_1_T1_0613_0000",
+    "barb_0001-4133_1920_num_6_1_T1_0012_0000"
+];
+
+// Création d'un objet contenant toutes les images
+const images = {};
+for (let name of imageNames) {
+    images[name] = require(`./assets/pages/${name}/${name}_original.png`);
+    images[`${name}_expert`] = require(`./assets/pages/${name}/${name}_resultat_expert.png`);
+    images[`${name}_v1`] = require(`./assets/pages/${name}/${name}_resultat_outil_v1.png`);
+    images[`${name}_v2`] = require(`./assets/pages/${name}/${name}_resultat_outil_v2.png`);
+    images[`${name}_v3`] = require(`./assets/pages/${name}/${name}_resultat_outil_v3.png`);
+}
+
+// Image par défaut TODO: à changer
+let imageSrc = images["barb_0001-4133_1919_num_5_1_T1_0021_0000"];
+
+// Modale de connexion
 function PasswordModal({setPassword, errorMessage}) {
     const [passwordInput, setPasswordInput] = useState('');
 
-    function handlePasswordChange(e) {
-        setPasswordInput(e.target.value);
-    }
+    const handlePasswordChange = (event) => {
+        setPasswordInput(event.target.value);
+    };
 
-    function handleSubmit(e) {
-        e.preventDefault();
+    const handlePasswordSubmit = (event) => {
+        event.preventDefault();
         setPassword(passwordInput);
-    }
+    };
 
     return (
         <div className="modal-background">
             <div className="modal">
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handlePasswordSubmit}>
                     <h2>Veuillez saisir le mot de passe :</h2>
                     {errorMessage && <p>{errorMessage}</p>}
                     <input type="password" value={passwordInput} onChange={handlePasswordChange}/>
+                    <br/>
+                    <button type="submit">Se connecter</button>
                 </form>
-                <button type="submit">Submit</button>
             </div>
         </div>
     );
@@ -43,17 +63,22 @@ export default function App() {
         }
     }, []);
 
-    function handleOpenModal() {
-        setIsModalOpen(true);
-    }
-
     function handleCloseModal() {
         setIsModalOpen(false);
     }
 
+    function handleOpenModal() {
+        setIsModalOpen(true);
+    }
+
+    function handleSelectChange(event) {
+        setSelectedImage(event.target.value);
+        imageSrc = images[event.target.value];
+    }
+
     function handlePasswordSubmit(submittedPassword) {
         if (submittedPassword === 'IopetiMathieuYvoz') {
-            Cookies.set('password', submittedPassword, {expires: 7}); // enregistre le mot de passe pour 7 jours
+            Cookies.set('password', submittedPassword, {expires: 1});
             setIsLoggedIn(true);
             setIsModalOpen(false);
         } else {
@@ -61,31 +86,48 @@ export default function App() {
         }
     }
 
-    function handleLogout() {
-        Cookies.remove('password');
-        setIsLoggedIn(false);
-    }
+    const [selectedImage, setSelectedImage] = useState(Object.keys(images)[0]);
 
     return (
         <div>
             {isLoggedIn ? (
                 <div>
                     <header>
-                        <Menu onOpenModal={handleOpenModal} onLogout={handleLogout}/>
+                        <div className="menu">
+                            {"Page sélectionnée : "}
+                            <select value={selectedImage} onChange={handleSelectChange}>
+                                {Object.keys(images).map((imageName) => (
+                                    <option key={imageName} value={imageName}>{imageName}</option>
+                                ))}
+                            </select>
+                        </div>
                     </header>
                     <main>
                         <div className="image">
-                            <Image/>
-                            <Button/>
+                            <div>
+                                <TransformWrapper defaultScale={1} defaultPositionX={1} defaultPositionY={1}>
+                                    <TransformComponent>
+                                        <img src={imageSrc} alt=""/>
+                                    </TransformComponent>
+                                </TransformWrapper>
+                            </div>
+                            <button className="button" type="button"> Celle de gauche !</button>
                         </div>
                         <div className="image">
-                            <Image/>
-                            <Button/>
+                            <div>
+                                <TransformWrapper defaultScale={1} defaultPositionX={1} defaultPositionY={1}>
+                                    <TransformComponent>
+                                        <img src={imageSrc} alt=""/>
+                                    </TransformComponent>
+                                </TransformWrapper>
+                            </div>
+                            <button className="button" type="button"> Celle de droite !</button>
                         </div>
                     </main>
                 </div>
             ) : (
                 <PasswordModal setPassword={handlePasswordSubmit} closeModal={handleCloseModal}
+                               openModal={handleOpenModal}
                                errorMessage={errorMessage}/>
             )}
         </div>
