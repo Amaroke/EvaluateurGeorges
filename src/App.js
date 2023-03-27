8694e608e7c5270287a285c6dffcb8ad62bbe394
PerryPerrz <iophugstud6@gmail.com>
2023-03-27 10:00:06 +0200

    Mise en place de la phase 2 + ratings des étoiles

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ for (let name of imageNames) {
     images[`${name}_extrapolation`] = require(`./assets/pages/${name}/${name}_extrapolation.png`);
 }
 
+const etoilePleine = require(`./assets/stars/etoile_pleine.png`);
+const etoileVide = require(`./assets/stars/etoile_vide.png`);
+
 let imageSrc = imageNames[0]
 
 // Modale de connexion
@@ -54,6 +57,14 @@ function PasswordModal({setPassword, errorMessage}) {
 
 export default function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isPhase2, setIsPhase2] = useState(false);
+
+    const [rating1, setRating1] = useState(false);
+    const [rating2, setRating2] = useState(false);
+    const [rating3, setRating3] = useState(false);
+    const [rating4, setRating4] = useState(false);
+    const [selectedRating, setSelectedRating] = useState(0);
+
     const [, setIsModalOpen] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
     const [imageScores, setImageScores] = useState({});
@@ -81,7 +92,7 @@ export default function App() {
     function handleImageScore(imageName) {
         setImageScores({
             ...imageScores,
-            [imageName]: (imageScores[imageName] || 0) + 1
+            [imageName]: (imageScores[imageName] || 0) + selectedRating
         });
         console.log(imageScores);
     }
@@ -116,6 +127,111 @@ export default function App() {
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
+    }
+
+    //Fonction permettant de passer à la seconde phase de l'évaluation.
+    function handleSecondPhase(){
+        // Sauvegarde du contenu de la page
+        const originalContent = document.body.innerHTML;
+
+        //Affiche de la phase 2
+        setIsPhase2(true)
+    }
+
+    function handleRating(val, value, select){
+        setSelectedRating(select)
+        switch (val) {
+            case 1:
+                setRating1(value && select !== val)
+                break;
+            case 2:
+                setRating1(value && select !== val)
+                setRating2(value && select !== val)
+                break;
+            case 3:
+                setRating1(value && select !== val)
+                setRating2(value && select !== val)
+                setRating3(value && select !== val)
+                break;
+            case 4:
+                setRating1(value && select !== val)
+                setRating2(value && select !== val)
+                setRating3(value && select !== val)
+                setRating4(value && select !== val)
+                break;
+        }
+    }
+
+    //Fonction permettant de gérer la note d'une image.
+    function starRating() {
+        //Gestion de la note.
+
+
+        //Description des étoiles.
+        const getStarDescription = (rating) => {
+            switch (rating) {
+                case 1:
+                    return "Immonde";
+                case 2:
+                    return "Très nul";
+                case 3:
+                    return "Super";
+                case 4:
+                    return "Parfait";
+                default:
+                    return "";
+            }
+        };
+
+        return (
+            <div className="button2-container">
+                <button className="button2"
+                    data-rating="1"
+                    onClick={() => handleRating(1, false, 1)}
+                    onMouseOver={() => handleRating(1,true, selectedRating)}
+                    onMouseOut={() => handleRating(1,false, selectedRating)}
+                >
+                    <img
+                        src={rating1 ? etoilePleine : etoileVide}
+                        alt="Étoile 1"
+                    />
+                </button>
+                <button className="button2"
+                    data-rating="2"
+                    onClick={() =>handleRating(2, false, 2)}
+                        onMouseOver={() => handleRating(2,true, selectedRating)}
+                        onMouseOut={() => handleRating(2,false, selectedRating)}
+                >
+                    <img
+                        src={rating2 ? etoilePleine : etoileVide}
+                        alt="Étoile 2"
+                    />
+                </button>
+                <button className="button2"
+                    data-rating="3"
+                    onClick={() => handleRating(3, false, 3)}
+                        onMouseOver={() => handleRating(3,true, selectedRating)}
+                        onMouseOut={() => handleRating(3,false, selectedRating)}
+                >
+                    <img
+                        src={rating3 ? etoilePleine : etoileVide}
+                        alt="Étoile 3"
+                    />
+                </button>
+                <button className="button2"
+                    data-rating="4"
+                    onClick={() => handleRating(4, false, 4)}
+                        onMouseOver={() => handleRating(4,true, selectedRating)}
+                        onMouseOut={() => handleRating(4,false, selectedRating)}
+                >
+                    <img
+                        src={rating4 ? etoilePleine : etoileVide}
+                        alt="Étoile 4"
+                    />
+                </button>
+                <p>{getStarDescription(rating)}</p>
+            </div>
+        );
     }
 
     const [selectedImage, setSelectedImage] = useState(Object.keys(images)[0]);
@@ -154,6 +270,56 @@ export default function App() {
     return (
         <div>
             {isLoggedIn ? (
+                isPhase2 ? (
+                        <div>
+                            <header>
+                                <div className="menu">
+                                    <button className="saveButton" type="button"
+                                            onClick={() => handleExport()}> Sauvegarder
+                                    </button>
+                                    {"Page sélectionnée : "}
+                                    <select value={selectedImage} onChange={handleSelectChange}>
+                                        {imageNames.map((imageName) => (
+                                            <option key={imageName} value={imageName}>{imageName}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                            </header>
+                            <main>
+                                <div className="image">
+                                    <TransformWrapper
+                                        defaultScale={1}
+                                        defaultPositionX={1}
+                                        defaultPositionY={1}
+                                        onWheel={(object) => handleZoomChange(object.state)}
+                                        onPanning={(object) => handleZoomChange(object.state)}
+                                        ref={wrapperRef0}
+                                    >
+                                        <TransformComponent>
+                                            <img src={images[img0]} alt=""/>
+                                        </TransformComponent>
+                                    </TransformWrapper>
+                                    <button className="button" type="button" disabled="true"> Image originale
+                                    </button>
+                                </div>
+                                <div className="image">
+                                    <TransformWrapper
+                                        defaultScale={1}
+                                        defaultPositionX={1}
+                                        defaultPositionY={1}
+                                        onWheel={(object) => handleZoomChange(object.state)}
+                                        onPanning={(object) => handleZoomChange(object.state)}
+                                        ref={wrapperRef1}
+                                    >
+                                        <TransformComponent>
+                                            <img src={images[img1]} alt=""/>
+                                        </TransformComponent>
+                                    </TransformWrapper>
+                                    {starRating()}
+                                </div>
+                            </main>
+                        </div>
+                    ):
                 <div>
                     <header>
                         <div className="menu">
@@ -219,13 +385,14 @@ export default function App() {
                                     onClick={() => handleImageScore(images[img2])}> Celle de droite !
                             </button>
                         </div>
+                        <button className="button" type="button" onClick={() => handleSecondPhase()}>Phase 2</button>
                     </main>
                 </div>
-            ) : (
+            ) :  (
                 <PasswordModal setPassword={handlePasswordSubmit} closeModal={handleCloseModal}
                                openModal={handleOpenModal}
                                errorMessage={errorMessage}/>
-            )}
+                )}
         </div>
     );
 }
